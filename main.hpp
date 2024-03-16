9978b7f3472eaca3532bbba1ed8f1f1901ef552f
Sayara02 <azizovna02@gmail.com>
2024-03-15 21:46:32 -0700

    build

--- main.hpp
+++ main.hpp
@@ -1,3 +1,13 @@
 /***************************************************
  * Code your program here
  ***************************************************/
+#include "main.cpp"
+using namespace std;
+int writeFile() {
+    ofstream file("employee.txt");
+    if (!file.is_open()) {
+        cerr << "Error: Unable to open file employee.txt" << endl;
+        return 0;
+    }
+}
+
