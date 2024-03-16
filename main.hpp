04bc3cec868ef9d75027d23f8139d2d89d505b49
Sayara02 <azizovna02@gmail.com>
2024-03-15 21:57:56 -0700

    run

--- main.hpp
+++ main.hpp
@@ -2,6 +2,8 @@
  * Code your program here
  ***************************************************/
 #include "main.cpp"
+#include <fstream>
+
 using namespace std;
 int writeFile() {
     ofstream file("employee.txt");
@@ -9,5 +11,26 @@ int writeFile() {
         cerr << "Error: Unable to open file employee.txt" << endl;
         return 0;
     }
+    int numEmployees;
+    cout << "Enter the number of employees: ";
+    cin >> numEmployees;
+    for(int i = 0; i < numEmployees; ++i) {
+        int id;
+        string name, department;
+        double salary;
+        cout << "Enter details for employee #" << i+1 << ":" << endl;
+        cout << "ID: ";
+        cin >> id;
+        cout << "Name: ";
+        cin >> name:
+        cout << department;
+        cin >> department;
+        cout << "Salary: ";
+        cin >> salary;
+        file << id << " " << name << " " << department << " " << salary << endl;
+
+        }
+        file.close();
+        return numEmployees;
 }
 
