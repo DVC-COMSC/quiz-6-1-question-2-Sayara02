35761828f838f01fc351d4d6c41f8d222516e848
Sayara02 <azizovna02@gmail.com>
2024-03-15 22:26:34 -0700

    erroes

--- main.cpp
+++ main.cpp
@@ -1,52 +1,10 @@
 #include "main.hpp"
-#include <string>
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
 #include <iostream>
+#include <fstream>
 #include <string>
-int writeFile() {
-    std::ofstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error: Unable to open file employee.txt" << filename << std::endl;
-        return 0;
-    }
-    int numEmployees;
-    std::cout << "Enter the number of employees: ";
-    std::cin >> numEmployees;
-    for(int i = 0; i < numEmployees; ++i) {
-        int id;
-        std::string name, department;
-        double salary;
-        std::cout << "Enter details for employee #" << i+1 << ":" << endl;
-        std::cout << "ID: ";
-        std::cin >> id;
-        std::cout << "Name: ";
-        std::cin >> name;
-        std::cout << department;
-        std::cin >> department;
-        std::cout << "Salary: ";
-        std::cin >> salary;
-        file << id << " " << name << " " << department << " " << salary << endl;
-
-        }
-        file.close();
-        return numEmployees;
-}
-int readFile() {
-    istd::fstream file("employee.txt");
-    if (!file.is_open()) {
-        std::cerr << "Error: Unable to open file employee.txt" << endl;
-        return 0;
-    }
-    int numEmployees = 0;
-    std::string line;
-    while (getline(file, line)) {
-        std::cout << line std::endl;
-        numEmployees++;
-    }
-    file.close();
-    return numEmployees;
-
-}
-
 int main()
 {
 
--- main.hpp
+++ main.hpp
@@ -2,12 +2,53 @@
  * Code your program here
  ***************************************************/
 
-#ifndef MAIN_HPP
-#define MAIN_HPP
+int writeFile(const std::string& filename) {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        return 0;
+    }
 
-#include <string>
+    int numEmployees;
+    std::cout << "Enter the number of employees: ";
+    std::cin >> numEmployees;
 
-int writeFile(const std::string &filename);
-int readFile(const std::string &filename);
+    for (int i = 0; i < numEmployees; ++i) {
+        int id;
+        std::string name, department;
+        double salary;
 
-#endif // MAIN_HPP;
+        std::cout << "Enter details for employee #" << i + 1 << ":" << std::endl;
+        std::cout << "ID: ";
+        std::cin >> id;
+        std::cout << "Name: ";
+        std::cin >> name;
+        std::cout << "Department: "; // Fixed missing output
+        std::cin >> department;
+        std::cout << "Salary: ";
+        std::cin >> salary;
+
+        file << id << " " << name << " " << department << " " << salary << std::endl;
+    }
+
+    file.close();
+    return numEmployees;
+}
+
+int readFile(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        return 0;
+    }
+
+    int numEmployees = 0;
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+        ++numEmployees;
+    }
+
+    file.close();
+    return numEmployees;
+}
